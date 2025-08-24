9f6aeb1b986c98cee2ee6a4f1b34979262a2e67d
ohhfishal <ohhfishal@gmail.com>
2025-08-24 13:43:03 -0700

    feat: logger config

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -11,6 +11,9 @@ import (
 
 type RootCmd struct {
 	Config kong.ConfigFlag `short:"c" help:"Path to config file to load." type:"path"`
+	LoggerConfig struct {
+		Level string `enum="disabled,error,warn,info,debug" placeholder:"info" default="info" help:"Log level to use. ('disabled','error','warn','info','debug')"`
+	} `embed:"" prefix:"logger-"`
 	Build  BuildCmd        `cmd:"" help:"Compile a resume from a input file."`
 	Serve  ServeCmd        `cmd:"" help:"Run resume-wizard as a local HTTP serve."`
 	Log    LogCmd          `cmd:"" help:"Log usuage of resumes."`
@@ -40,13 +43,32 @@ func Run(ctx context.Context, stdout io.Writer, args []string) error {
 		return err
 	}
 
-	logger := slog.New(slog.NewJSONHandler(stdout, &slog.HandlerOptions{
-		Level: slog.LevelInfo, // TODO: Make configurable
-	}))
+	logger := cmd.NewLogger(stdout)
 	slog.SetDefault(logger)
 
 	if err := context.Run(logger); err != nil {
 		return err
 	}
 	return nil
+}
+
+func (cmd RootCmd) NewLogger(stdout io.Writer) *slog.Logger {
+	var level slog.Level
+	switch cmd.LoggerConfig.Level {
+	case "disabled":
+		return slog.New(slog.DiscardHandler)
+	case "error":
+		level = slog.LevelError
+	case "warn":
+		level = slog.LevelWarn
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		fallthrough
+	default:
+		level = slog.LevelInfo
+	}
+	return  slog.New(slog.NewJSONHandler(stdout, &slog.HandlerOptions{
+		Level: level,
+	}))
 }
